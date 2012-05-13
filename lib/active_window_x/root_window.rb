9feb1aa9a81dd6fedf7f5c834a4cf09d45d324da
Keiichiro Ui <keiichiro.ui@gmail.com>
2012-05-13 16:49:38 +0900

    implement #active_window

--- lib/active_window_x/root_window.rb
+++ lib/active_window_x/root_window.rb
@@ -6,5 +6,15 @@ module ActiveWindowX
 
   # binding for a root Window on X11
   class RootWindow < Window
+
+    def active_window
+      prop_val = prop '_NET_ACTIVE_WINDOW'
+      if prop_val.nil? or prop_val.first == Xlib::None
+        nil
+      else
+        Window.new(@display, prop_val.first)
+      end
+    end
+
   end
 end
