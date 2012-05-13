e1f91ae26b86e69d94c24030a001725966e8d610
Keiichiro Ui <keiichiro.ui@gmail.com>
2012-05-13 11:22:50 +0900

    add a skelton of Window

--- lib/active_window_x.rb
+++ lib/active_window_x.rb
@@ -1,7 +1,9 @@
 # -*- coding:undecided-unix; mode:ruby; -*-
 
 require "active_window_x/version"
+require "active_window_x/window"
 
 module ActiveWindowX
   require "active_window_x/xlib"
+  class Window; end
 end
---
+++ lib/active_window_x/window.rb
@@ -0,0 +1,19 @@
+# -*- coding:utf-8; mode:ruby; -*-
+
+module ActiveWindowX
+
+  # binding for Window on X11
+  class Window
+
+    # a return value of XQueryTree
+    def root
+    end
+
+    def parent
+    end
+
+    def children
+    end
+  end
+
+end
