066797648761e4aa1f2924737a83284f3537388f
Keiichiro Ui <keiichiro.ui@gmail.com>
2012-05-13 11:49:58 +0900

    fix invalid mock

--- spec/window_spec.rb
+++ spec/window_spec.rb
@@ -6,47 +6,76 @@ include ActiveWindowX
 
 describe Xlib do
   before do
-    @display = mock Xlib::Display
+    @raw_display = mock Xlib::Display
+    @display = mock Display
+    @display.stub(:raw){@raw_display}
     @id = 123456
     @window = Window.new @display, @id
   end
 
   describe '#root' do
-    before do
-      @root = Window.new @display, 111
-      Xlib.should_receive(:x_query_tree).
-        with(@display, @window).
-        and_return([@root, nil, nil])
+    context ', which recive a window from x_query_tree,' do
+      before do
+        @root = 111
+        Xlib.should_receive(:x_query_tree).
+          with(@display.raw, @window.id).
+          and_return([@root, nil, []])
+      end
+      it 'should return the root Window' do
+        @window.root.id.should == @root
+      end
     end
-    it 'should return the root Window' do
-      @window.root.should == @root
+    context ', which recive nil from x_query_tree,' do
+      before do
+        Xlib.should_receive(:x_query_tree).
+          with(@display.raw, @window.id).
+          and_return([nil, nil, []])
+      end
+      it 'should return nil' do
+        @window.root.should be_nil
+      end
     end
   end
 
   describe '#parent' do
-    before do
-      @parent = Window.new @display, 111
-      Xlib.should_receive(:x_query_tree).
-        with(@display, @window).
-        and_return([nil, @parent, nil])
+    context ', which recive a window from x_query_tree,' do
+      before do
+        @parent = 111
+        Xlib.should_receive(:x_query_tree).
+          with(@display.raw, @window.id).
+          and_return([nil, @parent, []])
+      end
+      it 'should return the parent Window' do
+        @window.parent.id.should == @parent
+      end
     end
-    it 'should return the parent Window' do
-      @window.parent.should == @parent
+    context ', which recive nil from x_query_tree,' do
+      before do
+        Xlib.should_receive(:x_query_tree).
+          with(@display.raw, @window.id).
+          and_return([nil, nil, []])
+      end
+      it 'should return nil' do
+        @window.root.should be_nil
+      end
     end
   end
 
   describe '#children' do
     before do
       @children = []
-      @children.push Window.new @display, 111
-      @children.push Window.new @display, 222
-      @children.push Window.new @display, 333
+      @children.push 111
+      @children.push 222
+      @children.push 333
       Xlib.should_receive(:x_query_tree).
-        with(@display, @window).
+        with(@display.raw, @window.id).
         and_return([nil, nil, @children])
     end
     it 'should return the child Windows' do
-      @window.children.should == @children
+      c = @window.children
+      c[0].id.should == @children[0]
+      c[1].id.should == @children[1]
+      c[2].id.should == @children[2]
     end
   end
 end
