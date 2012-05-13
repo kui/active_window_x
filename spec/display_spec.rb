c731de2d937f3f94f6e29fe281db076950373806
Keiichiro Ui <keiichiro.ui@gmail.com>
2012-05-13 12:30:16 +0900

    add examples for Display#active_window

--- spec/display_spec.rb
+++ spec/display_spec.rb
@@ -20,9 +20,33 @@ describe Xlib do
     end
   end
 
-  describe 'active_window' do
+  describe '#active_window' do
     before do
+      @root_id = 123
+      @prop_atom = 111
       @id = 123456
+      Xlib.should_receive(:x_intern_atom).with(@display, '_NET_ACTIVE_WINDOW').
+        and_return(@prop_atom)
+    end
+    context ', which Xlib::x_get_window_property find an active window,' do
+      before do
+        Xlib.should_receive(:x_get_window_property).
+          with(@display, @root_id, @prop_atom, 0, Display::READ_BUFF_LENGTH, false, Xlib::AnyPropertyType).
+          and_return([0,32,2,0,[@id,0].pack('l!*')])
+      end
+      it 'should return the active windows' do
+        @display.active_window.id.should == @id
+      end
+    end
+    context ', which Xlib::x_get_window_property cannot find an active window,' do
+      before do
+        Xlib.should_receive(:x_get_window_property).
+          with(@display, @root_id, @prop_atom, 0, Display::READ_BUFF_LENGTH, false, Xlib::AnyPropertyType).
+          and_return([0,32,2,0,[Xlib::None,0].pack('l!*')])
+      end
+      it 'should return nil' do
+        @display.active_window.should == nil
+      end
     end
   end
 end
