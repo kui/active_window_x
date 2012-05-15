6a66fc0ad535ad39555b140f05fb88e9eb4afd13
Keiichiro Ui <keiichiro.ui@gmail.com>
2012-05-15 22:48:20 +0900

    fix bug

--- lib/active_window_x/property_event.rb
+++ lib/active_window_x/property_event.rb
@@ -2,34 +2,35 @@
 
 module ActiveWindowX
 
-  # the number of last request processed by server
-  attr_reader :serial
-
-  # true if this came from a SendEvent request
-  attr_reader :send_event
-
-  # Display the event was read from
-  attr_reader :display
-
-  # the window whose associated property was changed
-  attr_reader :window
-
-  # the property's atom and indicates which property was changed or desired
-  attr_reader :atom
-
-  # the server time when the property was changed
-  attr_reader :time
-
-  # * PropertyNewValue when a property of the window is changed using
-  #   XChangeProperty or XRotateWindowProperties (even when adding zero-length
-  #   data using XChangeProperty) and when replacing all or part of a property
-  #   with identical data using XChangeProperty or XRotateWindowProperties.
-  # * PropertyDelete when a property of the window is deleted using
-  #   XDeleteProperty or, if the delete argument is True, XGetWindowProperty
-  attr_reader :state
-
   # binding for XPropertyEvent on X11
   class PropertyEvent < Event
+
+    # the number of last request processed by server
+    attr_reader :serial
+
+    # true if this came from a SendEvent request
+    attr_reader :send_event
+
+    # Display the event was read from
+    attr_reader :display
+
+    # the window whose associated property was changed
+    attr_reader :window
+
+    # the property's atom and indicates which property was changed or desired
+    attr_reader :atom
+
+    # the server time when the property was changed
+    attr_reader :time
+
+    # * PropertyNewValue when a property of the window is changed using
+    #   XChangeProperty or XRotateWindowProperties (even when adding zero-length
+    #   data using XChangeProperty) and when replacing all or part of a property
+    #   with identical data using XChangeProperty or XRotateWindowProperties.
+    # * PropertyDelete when a property of the window is deleted using
+    #   XDeleteProperty or, if the delete argument is True, XGetWindowProperty
+    attr_reader :state
+
     def initialize display, raw
       super
       @serial = raw.serial
