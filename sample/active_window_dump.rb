17405bd4f4ae5c6b299532334cd455d2aaa0bccd
Keiichiro Ui <keiichiro.ui@gmail.com>
2012-05-17 00:18:58 +0900

    add a sample to show all properties of event.window

---
+++ sample/active_window_dump.rb
@@ -0,0 +1,30 @@
+# dump all properties of window on changing active window
+
+require 'active_window_x'
+
+@listener = ActiveWindowX::EventListener.new
+
+# when pressing Ctrl-C
+trap :INT do
+  @listener.destroy
+  exit true
+end
+
+@listener.start do |e|
+  next until e.window
+
+  w = e.window
+  puts <<__OUTPUT__ if e.type == :active_window
+######### change active window  #########
+     id:	#{w.id}
+     title:	#{w.title}
+     name:	#{w.app_name}
+     class:	#{w.app_class}
+     pid:	#{w.pid}
+     command:	#{w.command}
+__OUTPUT__
+  puts <<__OUTPUT__ if e.type == :title
+######### change title #########
+     title:	#{w.title}
+__OUTPUT__
+end
