0b5669ba2ac6f5590b76a7f4a455971918905fd2
Keiichiro Ui <keiichiro.ui@gmail.com>
2012-05-16 23:48:55 +0900

    more simplify

--- sample/simple.rb
+++ sample/simple.rb
@@ -2,7 +2,5 @@
 require 'active_window_x'
 
 ActiveWindowX::EventListener.new do |e|
-  puts(e.window ?
-       "#{e.type}:\t#{e.window.app_name}\t#{e.window.title}" :
-       "#{e.type}")
+  puts "#{e.type}:\t#{e.window}"
 end
