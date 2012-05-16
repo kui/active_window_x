c8f44e61cf6680c597a9b5bd5d9afe9e0d996cff
Keiichiro Ui <keiichiro.ui@gmail.com>
2012-05-17 01:40:32 +0900

    more

--- sample/simple.rb
+++ sample/simple.rb
@@ -2,5 +2,5 @@ require 'rubygems'
 require 'active_window_x'
 
 ActiveWindowX::EventListener.new do |e|
-  puts "#{e.type}:\t#{e.window ? e.window.name : nil}"
+  puts "#{e.type}:\t#{e.window ? e.window.title : nil}"
 end
